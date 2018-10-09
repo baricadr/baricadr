538abec1349635ae9f24dace2f3f6e93d2676261
root <root@archivedev.novalocal>
2018-10-09 09:23:19 +0000

    add comand rclone ftp

--- testrclone.py
+++ testrclone.py
@@ -1,34 +1,34 @@
 # -*- coding: utf-8 -*-
 
-from subprocess  import call
-from subprocess  import run
+from subprocess  import run, call, PIPE, Popen
 
-SRC_PATH="/root"
-DEST_PATH="/root"
-FILE="toto.txt"
+SRC_PATH="/tmp"
+DEST_PATH="/tmp/"
+FILE="dir"
 TYPE_TRANSFERT="copy" #move
+REMOTE="sftp"
+password='test293544'
+user='toto'
 
+#generate obscure password for connect to distant server
 try:
-    retcode = call('rclone ls sftp:'+SRC_PATH+'/'+FILE, shell=True)
+    p = Popen(['rclone', 'obscure', password], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    output, err = p.communicate(b"input data that is passed to subprocess' stdin")
+    retcode = p.returncode
+    obscure_password = output.decode('ascii')
     if retcode != 0:
-        raise RuntimeError("Child was terminated by signal " + str(retcode))
-        #3 : file or dir don't exist
+        raise RuntimeError("Child was terminated by signal " + str(retcode)+"can't generate password")
 except OSError as e:
     print("Execution failed:"+e, file=sys.stderr)
     sys.exit(1)
 
+#pull data with ftp
 try:
-    retcode = call('rclone '+TYPE_TRANSFERT+' sftp:'+SRC_PATH+'/'+FILE+' '+DEST_PATH, shell=True)
+    print('rclone '+TYPE_TRANSFERT+' '+SRC_PATH+'/'+FILE+' '+REMOTE+':'+DEST_PATH+' --sftp-user '+ user +' --sftp-pass ' + obscure_password)
+    retcode = call('rclone '+TYPE_TRANSFERT+' '+SRC_PATH+'/'+FILE+' '+REMOTE+':'+DEST_PATH+' --sftp-user '+ user +' --sftp-pass ' + obscure_password, shell=True)
     if retcode != 0:
-        raise RuntimeError("Child was terminated by signal " + str(retcode))
+        raise RuntimeError("Child was terminated by signal " + str(retcode)+"can't upload" + SRC_PATH +  '/' +FILE)
+        #ERROR_3 : file or dir don't exist
 except OSError as e:
     print("Execution failed:"+e, file=sys.stderr)
     sys.exit(1)
-
-
-"""if subprocess.check_output(['rclone', 'ls','sftp:'+PATH+'/'+FILE], check=True):
-    subprocess.run(['rclone', 'ls','sftp:'+PATH])
-else:
-    print("error")
-#subprocess.run(['rclone','move',PATH+'/'+FILE,'sftp:'+PATH])
-#subprocess.run(['rclone', 'ls','sftp:'+PATH+'/'+FILE])"""
