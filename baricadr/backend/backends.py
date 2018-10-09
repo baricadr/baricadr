3db63d322690e891596bd2cfc3c7a957e78d790f
Anthony Bretaudeau <anthony.bretaudeau@inria.fr>
2018-10-09 11:48:06 +0200

    stuff

--- baricadr/backend/backends.py
+++ baricadr/backend/backends.py
@@ -1,3 +1,6 @@
+from subprocess import PIPE, Popen, call
+
+
 class Backends():
     def __init__(self):
         self.backends = {
@@ -14,35 +17,62 @@ class Backends():
 
 
 class Backend():
-    def __init__(self, url, user, password, exclude):
+    def __init__(self, local_path, url, user, password, exclude):
         self.name = None
         self.url = url
         self.user = user
         self.password = password
         self.exclude = exclude
 
-    def pull():
+    def pull(self, path):
+        """
+        Download a file from remote into local repository
+
+        :type path: str
+        :param path: path to pull, without local or remote prefix
+
+        :rtype: ?
+        :return: ?
+        """
         raise NotImplementedError()
 
 
-class RcloneBackend():
+class RcloneBackend(Backend):
     def __init__(self, url, user, password, exclude):
         RcloneBackend.__init__(self, url, user, password, exclude)
 
+    def obscurify_password(self, clear_pass):
+        """
+        Generate obscure password to connect to distant server
+        """
+        p = Popen(['rclone', 'obscure', clear_pass], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        output, err = p.communicate()
+        retcode = p.returncode
+        obscure_password = output.decode('ascii')
+        if retcode != 0:
+            raise RuntimeError("Child was terminated by signal " + str(retcode) + ": can't obscurify password")
 
-class SftpBackend():
+        return obscure_password
+
+
+class SftpBackend(RcloneBackend):
     def __init__(self, url, user, password, exclude):
         RcloneBackend.__init__(self, url, user, password, exclude)
         self.name = 'sftp'
 
-    def pull():
-        print('Here I call rclone ...')
+    def pull(self, path):
+        obscure_password = self.obscurify_password(self.password)
+        # TODO create temp file for config (https://stackabuse.com/the-python-tempfile-module/) + use --config
+        retcode = call('rclone copy ' + SRC_PATH + '/' + path + ' ' + REMOTE + ':' + path + ' --sftp-user ' + user + ' --sftp-pass ' + obscure_password, shell=True)
+        if retcode != 0:
+            raise RuntimeError("Child was terminated by signal " + str(retcode) + ": can't copy " + SRC_PATH +  '/' +FILE)
+            # ERROR_3 : file or dir don't exist
 
 
-class S3Backend():
+class S3Backend(RcloneBackend):
     def __init__(self, url, user, password, exclude):
         RcloneBackend.__init__(self, url, user, password, exclude)
         self.name = 's3'
 
-    def pull():
+    def pull(self):
         print('Here I call rclone ...')
