989669ad4f276126725b04932728b0cdd556ac28
Anthony Bretaudeau <anthony.bretaudeau@inria.fr>
2018-11-23 17:39:23 +0100

    new test

--- tests/test_api.py
+++ tests/test_api.py
@@ -183,3 +183,87 @@ class TestApi(BaricadrTestCase):
             wait += 1
 
         assert response.json == {'finished': 'true', 'error': 'false', 'info': None}
+
+        assert os.path.exists(repo_dir + '/subfile.txt')
+        assert os.path.isdir(repo_dir + '/subsubdir')
+        assert os.path.exists(repo_dir + '/subsubdir/subsubfile.txt')
+
+        if os.path.exists(repo_dir):
+            shutil.rmtree(repo_dir)
+
+    def test_pull_local_add(self, app, client):
+
+        repo_dir = '/repos/test_repo/subdir'
+        if os.path.exists(repo_dir):
+            shutil.rmtree(repo_dir)
+
+        data = {
+            'path': repo_dir
+        }
+        response = client.post('/pull', json=data)
+
+        assert response.status_code == 200
+        assert 'task' in response.json
+
+        pull_id = response.json['task']
+
+        # Wait for the task to run
+        wait = 0
+        while wait < 10:
+            sleep(2)
+
+            response = client.get('/status/%s' % pull_id)
+
+            assert response.status_code == 200
+
+            if response.json['finished'] == "true":
+                break
+            else:
+                assert response.json['error'] == 'false'
+            wait += 1
+
+        assert response.json == {'finished': 'true', 'error': 'false', 'info': None}
+
+        # Copy a file in local repo
+        shutil.copyfile(repo_dir + '/subfile.txt', repo_dir + '/local_new_file.txt')
+
+        # Try to pull a file already pulled just before
+        data = {
+            'path': repo_dir + '/subsubdir'
+        }
+        response = client.post('/pull', json=data)
+
+        assert response.status_code == 200
+        assert 'task' in response.json
+
+        pull_id = response.json['task']
+
+        # Wait for the task to run
+        wait = 0
+        while wait < 10:
+            sleep(2)
+
+            response = client.get('/status/%s' % pull_id)
+
+            assert response.status_code == 200
+
+            if response.json['finished'] == "true":
+                break
+            else:
+                assert response.json['error'] == 'false'
+            wait += 1
+
+        assert response.json == {'finished': 'true', 'error': 'false', 'info': None}
+
+        assert os.path.exists(repo_dir + '/subfile.txt')
+        assert os.path.isdir(repo_dir + '/subsubdir')
+        assert os.path.exists(repo_dir + '/subsubdir/subsubfile.txt')
+        assert os.path.exists(repo_dir + '/local_new_file.txt')
+
+        if os.path.exists(repo_dir):
+            shutil.rmtree(repo_dir)
+
+
+# TODO test local modification of a file, local deletion of a file
+# TODO test checksum
+# TODO test emails
