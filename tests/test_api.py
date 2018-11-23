025fb11c6c529277d8df6a8c05ce299e463a2b27
Anthony Bretaudeau <anthony.bretaudeau@inria.fr>
2018-11-23 17:03:30 +0100

    test multiple pulls

--- tests/test_api.py
+++ tests/test_api.py
@@ -72,3 +72,114 @@ class TestApi(BaricadrTestCase):
 
         if os.path.exists(repo_dir):
             shutil.rmtree(repo_dir)
+
+    def test_pull_race(self, app, client):
+
+        repo_dir = '/repos/test_repo/subdir'
+        if os.path.exists(repo_dir):
+            shutil.rmtree(repo_dir)
+
+        data = {
+            'path': repo_dir
+        }
+        response = client.post('/pull', json=data)
+
+        assert response.status_code == 200
+        assert 'task' in response.json
+
+        pull_id = response.json['task']
+
+        # Ask immediately, the first one should not be finished yet
+        response = client.post('/pull', json=data)
+
+        assert response.status_code == 200
+        assert 'task' in response.json
+
+        pull_id_2 = response.json['task']
+
+        assert pull_id == pull_id_2
+
+        # Wait for the task to run
+        wait = 0
+        while wait < 10:
+            sleep(2)
+
+            response = client.get('/status/%s' % pull_id)
+
+            assert response.status_code == 200
+
+            if response.json['finished'] == "true":
+                break
+            else:
+                assert response.json['error'] == 'false'
+            wait += 1
+
+        assert response.json == {'finished': 'true', 'error': 'false', 'info': None}
+
+        assert os.path.exists(repo_dir + '/subfile.txt')
+        assert os.path.isdir(repo_dir + '/subsubdir')
+        assert os.path.exists(repo_dir + '/subsubdir/subsubfile.txt')
+
+        if os.path.exists(repo_dir):
+            shutil.rmtree(repo_dir)
+
+    def test_pull_twice(self, app, client):
+
+        repo_dir = '/repos/test_repo/subdir'
+        if os.path.exists(repo_dir):
+            shutil.rmtree(repo_dir)
+
+        data = {
+            'path': repo_dir
+        }
+        response = client.post('/pull', json=data)
+
+        assert response.status_code == 200
+        assert 'task' in response.json
+
+        pull_id = response.json['task']
+
+        # Wait for the task to run
+        wait = 0
+        while wait < 10:
+            sleep(2)
+
+            response = client.get('/status/%s' % pull_id)
+
+            assert response.status_code == 200
+
+            if response.json['finished'] == "true":
+                break
+            else:
+                assert response.json['error'] == 'false'
+            wait += 1
+
+        assert response.json == {'finished': 'true', 'error': 'false', 'info': None}
+
+        # Try to pull a file already pulled just before
+        data = {
+            'path': repo_dir + '/subsubdir'
+        }
+        response = client.post('/pull', json=data)
+
+        assert response.status_code == 200
+        assert 'task' in response.json
+
+        pull_id = response.json['task']
+
+        # Wait for the task to run
+        wait = 0
+        while wait < 10:
+            sleep(2)
+
+            response = client.get('/status/%s' % pull_id)
+
+            assert response.status_code == 200
+
+            if response.json['finished'] == "true":
+                break
+            else:
+                assert response.json['error'] == 'false'
+            wait += 1
+
+        assert response.json == {'finished': 'true', 'error': 'false', 'info': None}
