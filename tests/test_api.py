a979f5c67048f55ee11a673e96f2431727bbbf22
Anthony Bretaudeau <anthony.bretaudeau@inria.fr>
2018-11-29 16:49:17 +0100

    test local modification

--- tests/test_api.py
+++ tests/test_api.py
@@ -230,6 +230,40 @@ class TestApi(BaricadrTestCase):
         if os.path.exists(repo_dir):
             shutil.rmtree(repo_dir)
 
+    def test_pull_local_old(self, app, client):
+        """
+        Try to pull a dir containing some locally deleted data
+        """
+
+        repo_dir = '/repos/test_repo/subdir'
+        if os.path.exists(repo_dir):
+            shutil.rmtree(repo_dir)
+
+        self.pull_and_wait(client, repo_dir)
+
+        with open(repo_dir + '/subfile.txt', 'r') as local_file:
+            assert local_file.readline() == 'subfile content\n'
+
+        # Modify in local repo and change its modification time
+        with open(repo_dir + '/subfile.txt', 'w') as local_file:
+            local_file.write('This was touched locally\n')
+
+        new_time = os.stat(repo_dir + '/subfile.txt').st_mtime - 100000000
+        os.utime(repo_dir + '/subfile.txt', (new_time, new_time))
+
+        # Try to pull a dir already pulled just before
+        self.pull_and_wait(client, repo_dir)
+
+        assert os.path.exists(repo_dir + '/subfile.txt')
+        assert os.path.isdir(repo_dir + '/subsubdir')
+        assert os.path.exists(repo_dir + '/subsubdir/subsubfile.txt')
+
+        with open(repo_dir + '/subfile.txt', 'r') as local_file:
+            assert local_file.readline() == 'This was touched locally\n'
+
+        if os.path.exists(repo_dir):
+            shutil.rmtree(repo_dir)
+
     def pull_and_wait(self, client, path):
         data = {
             'path': path
@@ -258,9 +292,9 @@ class TestApi(BaricadrTestCase):
 
         assert response.json == {'finished': 'true', 'error': 'false', 'info': None}
 
-# TODO test local modification but older file
-# TODO test pulling /
+# TODO test pulling at higher or lower level than already pulling
 # TODO test checksum
 # TODO test emails
 # TODO test excludes
 # TODO implement freezing tasks
+# TODO force option for pull to allow replacing locally modified file?
