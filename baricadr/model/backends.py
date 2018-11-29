86cd02e59bc2cd3db2dbf5e01f6c3b5683a630c9
Anthony Bretaudeau <anthony.bretaudeau@inria.fr>
2018-11-29 17:15:26 +0100

    old stuff

--- baricadr/model/backends.py
+++ baricadr/model/backends.py
@@ -96,7 +96,7 @@ class SftpBackend(RcloneBackend):
         rel_path = repo.relative_path(path)
 
         src = "%s:%s%s" % (self.name, self.remote_prefix, rel_path)
-        # TODO detect if file or dir  -> WHY? I don't remember
+
         dest = "%s" % (path)
         # We use --ignore-existing to avoid deleting locally modified files (for example if a file was modified locally but the backup is not yet up-to-date)
         cmd = "rclone copy --ignore-existing --config '%s' '%s' '%s' --sftp-user '%s' --sftp-pass '%s'" % (tempRcloneConfig.name, src, dest, self.user, obscure_password)
