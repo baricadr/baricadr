7d374921b7b88a44b9a0e847a5c50cffcddda28a
Anthony Bretaudeau <anthony.bretaudeau@inria.fr>
2021-02-09 13:51:02 +0100

    change only access time during tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -11,8 +11,10 @@ class BaricadrTestCase():
             for root, subdirs, files in os.walk(path):
                 for name in files:
                     candidate = os.path.join(root, name)
+                    mod_time = os.stat(candidate).st_mtime
                     old_time = os.stat(candidate).st_atime - age
-                    os.utime(candidate, (old_time, old_time))  # TODO [HI] leave utime unmodified
+                    os.utime(candidate, (old_time, mod_time))
         else:
+            mod_time = os.stat(candidate).st_mtime
             old_time = os.stat(path).st_atime - age
-            os.utime(path, (old_time, old_time))  # TODO [HI] leave utime unmodified
+            os.utime(path, (old_time, mod_time))
