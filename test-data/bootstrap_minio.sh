2939ec973cee44c42955be5ad9b98532ce1dc466
Anthony Bretaudeau <anthony.bretaudeau@inria.fr>
2021-06-24 15:37:52 +0200

    tty

--- test-data/bootstrap_minio.sh
+++ test-data/bootstrap_minio.sh
@@ -2,8 +2,8 @@
 
 # A script to initialize a test bucket in a Minio container using a minioc container
 
-docker-compose -f docker-compose.dev.yml exec minioc mc alias set minio http://minio:9000 admin password
+docker-compose -f docker-compose.dev.yml exec -T minioc mc alias set minio http://minio:9000 admin password
 
-docker-compose -f docker-compose.dev.yml exec minioc mc mb minio/remote-test-repo
+docker-compose -f docker-compose.dev.yml exec -T minioc mc mb minio/remote-test-repo
 
-docker-compose -f docker-compose.dev.yml exec minioc mc cp -r test-repo minio/remote-test-repo
+docker-compose -f docker-compose.dev.yml exec -T minioc mc cp -r test-repo minio/remote-test-repo
